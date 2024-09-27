3896e7dd92e851c4bcb35640dff2d9cab3c218ad
Guilherme Oliveira <guideoliveira2197@gmail.com>
2024-09-27 19:23:07 -0300

    adcionando curriculo

--- src/js/script.js
+++ src/js/script.js
@@ -1,14 +1,14 @@
 document.getElementById('downloadBtn').addEventListener('click', function() {
-    // Criar um link temporário
+    
     const link = document.createElement('a');
     
-    // Definir o URL do arquivo PDF do currículo
-    link.href = '../Curriculo(2).pdf'; // Substitua pelo caminho correto do seu arquivo PDF
     
-    // Definir o nome do arquivo que será baixado
+    link.href = 'https://drive.google.com/file/d/1hweAOkZCxi4CCiRafMykWT7GS4XvVoMm/view?usp=sharing'; 
+    
+    
     link.download = 'Curriculo_Guilherme.pdf';
 
-    // Simular o clique no link
+    
     link.click();
   });
 
